eb2c2ee0d0ec3dd11b7bbc8d36eedc5ae2e41ad9
tgiqfe <tqji31001@gmail.com>
2022-07-13 19:17:24 +0900

    Add converting project.

---
+++ PS1Convert/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+string outputPath = @"..\..\..\..\PingMonitor\PingMonitor.ps1";
+string targetDir = @"..\..\..\..\PingMonitor";
+
+
+foreach (string file in Directory.GetFiles(targetDir, "*.cs", SearchOption.AllDirectories))
+{
+    string fileName = Path.GetFileName(file);
+    if(fileName == "Program.cs")
+    {
+        continue;
+    }
+
+
+}
+
+
+
+Console.ReadLine();
